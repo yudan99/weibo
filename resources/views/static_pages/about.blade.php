dcbb741d42c56d11f6ab66181862355cf85bc3af
yaoxin <2222122099@qq.com>
2020-03-22 09:44:55 +0000

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,15 @@
+<!-- <!DOCTYPE html>
+<html>
+<head>
+  <title>Weibo App</title>
+</head>
+<body>
+  <h1>关于页</h1>
+</body>
+</html> -->
+
+@extends('layouts.default')
+@section('title','关于我')
+@section('content')
+<h1>关于</h1>
+@stop
