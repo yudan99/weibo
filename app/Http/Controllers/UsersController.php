799b610742ac0be5b4f26bf5847897b3d15af942
yaoxin <2222122099@qq.com>
2020-03-24 08:51:54 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,9 +37,30 @@ class UsersController extends Controller
 
     }
 
-    public function destroy(){
-        Auth::logout();
-        session()->flash('success','您已成功退出');
-        return redirect('login');
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
     }
+
+    public function update(User $user,Request $request){
+
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'password'=>'nullable|confirmed|min:6'
+        ]);
+
+        $data=[];
+        $data['name']=$request->name;
+        if ($request->password) {
+            $data['password']=bcrypt($request->password);
+            # code...
+        }
+
+        $user->update($data);
+
+        session()->flash('success','个人资料更新成功');
+
+        return redirect()->route('users.show',$user);
+    }
+
+
 }
