3bfbc813e97405e062a97a5aa7b9397e4196e1de
yaoxin <2222122099@qq.com>
2020-03-24 17:49:16 +0000

    管理员可删除用户

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -17,6 +17,10 @@ class UserPolicy
     public function update(User $currentUser,User $user)
     {
         //
-        return $currentUser->id == $user->id;
+        return $currentUser->id === $user->id;
+    }
+
+    public function destroy(User $currentUser,User $user){
+        return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'yaoxin';
         $user->email = 'yaoxin@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
